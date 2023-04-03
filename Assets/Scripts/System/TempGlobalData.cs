12a68c026726c7471d17cb902e098952bb1c5973
Donghyun Jo <donghyun@bagelcode.com>
2023-04-03 23:44:39 +0900

    update temp data

---
+++ Assets/Scripts/System/TempGlobalData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGlobalData : MonoWeakSingleton<TempGlobalData>
+{
+    public ServantInfo openedServantInfo;
+}
